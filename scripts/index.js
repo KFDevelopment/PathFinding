7d8b5bc067d74198fe3ba41e614e0f95a870e820
superuserC <kevin.danthine@gmail.com>
2018-11-17 23:41:44 +0100

    A* implementation

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,12 @@ var gridTwoDim = [];
 var currentPositionX = 0;
 var currentPositionY = 0;
 var actualCursors = 0;
+var openList = [];
+var closedList = [];
 
 var maxPositionY = 0;
+
+const reducer = (min, currentValue) => currentValue.heuristicDistance < min.heuristicDistance ? currentValue : min;
 
   $(function(){
     redrawGrid(gridSize);
@@ -16,7 +20,6 @@ var maxPositionY = 0;
   $("body").on("drag", function(e) {
     e.preventDefault();
   })
-
 
 
   function redrawGrid(gridSize){
@@ -32,7 +35,7 @@ var maxPositionY = 0;
       for (var j = 0; j < gridSize; j++) {
         gridLine += "<td id="+globalIndex+"  onmouseup=\"unClick()\" onmousedown=\"clickCell("+globalIndex+")\" onmouseover=\"enterCell("+globalIndex+")\"></td>";
 
-        //status : 0 => empty, 1=> block, 2 => full
+        //status : 0 => empty, 1=> block, 2 => full, 3 => start, 4 => end
         grid.push({id:globalIndex, x:j, y:i, status:0});
         gridTwoDim[i].push(0);
         globalIndex += 1;
@@ -49,7 +52,57 @@ var maxPositionY = 0;
   });
 
 function clickCell(id){
-    if($("#" + id).hasClass("block")){
+
+  if(actualCursors == 0){
+      $("#" + id).removeClass("block");
+      $("#" + id).removeClass("end");
+      $("#" + id).addClass("start");
+
+      var cellToRedraw = grid.find(x => x.status == 3);
+      if(typeof cellToRedraw !=  'undefined'){
+        cellToRedraw.status = 0;
+        gridTwoDim[cellToRedraw.y][cellToRedraw.x] = 0;
+        $("#" + cellToRedraw.id).removeClass("start");
+      }
+
+      var cell = grid.find(x => x.id == id);
+      cell.status = 3;
+      gridTwoDim[cell.y][cell.x] = 3;
+  }
+  else if(actualCursors == 2){
+    $("#" + id).removeClass("block");
+    $("#" + id).removeClass("start");
+    $("#" + id).addClass("end");
+
+    var cellToRedraw = grid.find(x => x.status == 4);
+    if(typeof cellToRedraw !=  'undefined'){
+      cellToRedraw.status = 0;
+      gridTwoDim[cellToRedraw.y][cellToRedraw.x] = 0;
+      $("#" + cellToRedraw.id).removeClass("end");
+    }
+
+    var cell = grid.find(x => x.id == id);
+    cell.status = 4;
+    gridTwoDim[cell.y][cell.x] = 4;
+  }
+  else{
+    if($("#" + id).hasClass("start")){
+      $("#" + id).removeClass("start");
+      $("#" + id).addClass("block");
+
+      var index = grid.findIndex(x => x.id == id);
+      grid[id].status = 1;
+      gridTwoDim[grid[id].y][grid[id].x] = 1;
+    }
+    else if($("#" + id).hasClass("end")){
+      $("#" + id).removeClass("end");
+      $("#" + id).addClass("block");
+
+      var index = grid.findIndex(x => x.id == id);
+      grid[id].status = 1;
+      gridTwoDim[grid[id].y][grid[id].x] = 1;
+    }
+    else if($("#" + id).hasClass("block")){
       $("#" + id).removeClass("block");
       var cell = grid.find(x => x.id == id);
       cell.status = 0;
@@ -63,13 +116,12 @@ function clickCell(id){
 
     }
     clickToDrag = true;
+  }
 }
+
 function enterCell(id){
-  if(clickToDrag){
-    var index = grid.findIndex(x => x.id == id);
-    grid[id].status = 1;
-    gridTwoDim[grid[id].y][grid[id].x] = 1;
-    $("#" + id).addClass("block");
+  if(clickToDrag && actualCursors == 1){
+    clickCell(id);
   }
 }
 
@@ -77,12 +129,84 @@ function unClick(){
   clickToDrag = false;
 }
 
-$("#start").on("click", function(){
-  findStartCursorPosition();
-  console.log("start");
-})
-
 function switchCursor(id){
   actualCursors = id;
+}
+
+function pathFinding(){
+  var startCell = grid.find(x => x.status == 3);
+  var endCell = grid.find(x => x.status == 4);
+
+  var startCellCost = 0;
+  var startCellDistanceFromEnd = Math.abs(endCell.x - startCell.x) + Math.abs(endCell.y - startCell.y);
+  var startCellHeuristicDistance = startCellDistanceFromEnd;
+
+  openList.push({
+    id : startCell.id,
+    x : startCell.x,
+    y : startCell.y,
+    cost : 0,
+    heuristicDistance : startCellHeuristicDistance,
+    previousCellId : null
+  });
+var currentCell;
+  while(openList.length > 0){
+
+    currentCell = openList.reduce(reducer, {heuristicDistance : Number.MAX_SAFE_INTEGER});
+
+    console.log(currentCell);
+
+    if(currentCell.id == endCell.id){
+      break;
+    }
+
+    var neighboursCell = grid.filter(x =>
+       (x.x == currentCell.x && x.y == (currentCell.y - 1 ) ||
+       x.x == currentCell.x && x.y == (currentCell.y + 1 ) ||
+       x.x == (currentCell.x - 1) && x.y == (currentCell.y - 1 ) ||
+       x.x == (currentCell.x + 1) && x.y == (currentCell.y - 1 ) ||
+       x.x == (currentCell.x - 1) && x.y == currentCell.y ||
+       x.x == (currentCell.x + 1) && x.y == currentCell.y ||
+       x.x == (currentCell.x - 1) && x.y == (currentCell.y + 1 ) ||
+       x.x == (currentCell.x + 1) && x.y == (currentCell.y + 1 )
+     ) && x.status != 1);
+
+     console.log(neighboursCell);
+
+     neighboursCell.forEach(function(neighbour){
+
+       var A = closedList.find(x => x.id == neighbour.id);
+       var B = openList.find(x => x.id == neighbour.id);
+        if(typeof  A != 'undefined'){
+          if(A.cost < currentCell.cost){console.log("A");}
+        }
+        else if(typeof B != 'undefined'){
+          if(B.cost < currentCell.cost){console.log("B");}
+        }
+        else{
+          openList.push({
+            id : neighbour.id,
+            x : neighbour.x,
+            y : neighbour.y,
+            cost : currentCell.cost + 1,
+            heuristicDistance : currentCell.cost + Math.abs(endCell.x - neighbour.x) + Math.abs(endCell.y - neighbour.y),
+            previousCell : currentCell
+          });
+        }
+     })
+     closedList.push(currentCell);
+     openList = openList.filter(x => x.id != currentCell.id);
+  }
+drawPath(currentCell);
+console.log(closedList);
+
+}
+
+function drawPath(cell){
+  if(cell.id != grid.find(x => x.status == 3).id){
+      $("#" + cell.id).css("background-color","orange");
+      drawPath(cell.previousCell);
+  }
+
 
 }
